06fa2ad818d2d8d7e479cb663fe56dc56ce0feb2
agniksaha <agniksaha9953@gmail.com>
2025-09-19 05:43:27 +0530

    useRef used

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,43 +1,43 @@
 import style from "./AddTodo.module.css";
 import { IoIosAddCircleOutline } from "react-icons/io";
-import { useState } from "react";
+import { useRef } from "react";
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
-  const handleAddButtonClicked = () => {
+  // const [todoName, setTodoName] = useState("");
+  // const [dueDate, setDueDate] = useState("");
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  // };
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  // };
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(todoName, dueDate);
-    setDueDate("");
-    setTodoName("");
   };
   return (
-    <div className={`${style.myRow} row`}>
+    <form className={`${style.myRow} row`} onSubmit={handleAddButtonClicked}>
       <div className="col-6">
         <input
           type="text"
           placeholder="Enter todo here"
-          value={todoName}
-          onChange={handleNameChange}
+          ref={todoNameElement}
         />
       </div>
-      <div className={`  col-4`}>
-        <input type="date" value={dueDate} onChange={handleDateChange} />
+      <div className={`col-4`}>
+        <input type="date" ref={dueDateElement} />
       </div>
       <div className="col-2">
-        <button
-          type="button"
-          className={`${style.myButton} btn btn-success`}
-          onClick={handleAddButtonClicked}
-        >
+        <button type="submit" className={`${style.myButton} btn btn-success`}>
           <IoIosAddCircleOutline />
         </button>
       </div>
-    </div>
+    </form>
   );
 }
 
